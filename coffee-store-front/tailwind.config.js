2787e6e99ca21f9655ad80f6ca1f87ebbbbc418e
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-14 16:26:04 +0600

    color palette

--- coffee-store-front/tailwind.config.js
+++ coffee-store-front/tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      gray: {
+        100: "#F7F7F7",
+        200: "#ECEAE3",
+        300: "#5C5B5B",
+      },
+      white: "#FFF",
+      dark: "#1B1A1A",
+      primary: "#331A15",
+      accent: "#D2B48C",
+    },
     extend: {},
   },
   plugins: [],
