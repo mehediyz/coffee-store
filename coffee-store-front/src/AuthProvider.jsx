1452f5d6870359e1dbb48bd78d4339650a2997f5
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-15 21:18:50 +0600

    auth provider

---
+++ coffee-store-front/src/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "./firebase/firebaseConfig.js";
+const auth = getAuth(app);
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const userInfo = {
+    user,
+  };
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
